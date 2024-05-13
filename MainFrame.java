dc1b10ce52376fd0b893f8131e32c93e1dae2432
Nevinkai <nevin@tonyandmarlo.com>
2024-05-13 14:26:57 -0600

    test

--- MainFrame.java
+++ MainFrame.java
@@ -8,7 +8,7 @@ public class MainFrame extends JFrame{
     JMenuBar menuBar;
 
     MainFrame(){
-        // Testing
+        // Testingg dsg
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(new GridBagLayout());
